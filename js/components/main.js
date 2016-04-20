7aa41b900a6c24e7422cf3170f950ffe650b7cd7
Oana Peres <oana.peres@evozon.com>
2016-04-20 16:05:47 +0300

    center template

--- js/components/main.js
+++ js/components/main.js
@@ -8,15 +8,12 @@ class DashboardMain extends React.Component {
 
     render() {
         return (
-          <Block classes={'e-background-green-200'}>
-          <Block classes={'e-container e-background-green-200'}>
-              <Block classes={'e-container'} style={{margin: '100px 0 0 0'}}>
+          <Block>
+              <Block classes={'e-container'}>
                 <Block classes={'dashboard-main e-background-white'}>
                   <DashboardHeader />
-
+                </Block>
               </Block>
-            </Block>
-          </Block>
           </Block>
         );
     }
