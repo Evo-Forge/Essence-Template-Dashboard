8ee3387c3ab3058f424b918b2cfe9cc3cc3241c3
radufarcas10 <f.radu10@yahoo.com>
2016-04-20 16:06:43 +0300

    modified readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Implementing UI Kit - based on Essence ReactJS Framework
 
 ### The template
-Inspired from UI8.net - [Materiable UI Kit](https://ui8.net/products/materiable-web-ui-kit)
+Inspired from dribbble.com - [Dashboard Mail Concept](https://dribbble.com/shots/2625733-Mail-Concept-App/attachments/525150)
--- js/components/main.js
+++ js/components/main.js
@@ -9,14 +9,12 @@ class DashboardMain extends React.Component {
     render() {
         return (
           <Block classes={'e-background-green-200'}>
-          <Block classes={'e-container e-background-green-200'}>
-              <Block classes={'e-container'} style={{margin: '100px 0 0 0'}}>
-                <Block classes={'dashboard-main e-background-white'}>
-                  <DashboardHeader />
+            <Block classes={'e-container'} style={{margin: '100px 0 0 0'}}>
+              <Block classes={'dashboard-main e-background-white'}>
+                <DashboardHeader />
 
               </Block>
             </Block>
-          </Block>
           </Block>
         );
     }
